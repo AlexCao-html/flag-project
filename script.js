b6fbd5abb5ddfdca6fd84c08e32db20711a0753b
AlexCao-html <bubocao@outlook.com>
2023-05-17 23:13:45 -0700

    did the pages until the 15 star flag

---
+++ script.js
@@ -0,0 +1,24 @@
+$("#Grand-Union-Flag").click(() => {
+    $("main section").css("display", "none")
+    $("#Grand-Union-Flag-Sect").css("display", "grid")
+})
+
+$("#Pine-Tree-Flag").click(() => {
+    $("main section").css("display", "none")
+    $("#Pine-Tree-Flag-Sect").css("display", "grid")
+})
+
+$("#13-stars").click(() => {
+    $("main section").css("display", "none")
+    $("#13-Star-Sect").css("display", "grid")
+})
+
+$("#Battle-of-Bennington").click(() => {
+    $("main section").css("display", "none")
+    $("#Battle-of-Bennington-Sect").css("display", "grid")
+})
+
+$("#15-stars").click(() => {
+    $("main section").css("display", "none")
+    $("#15-Star-Sect").css("display", "grid")
+})
