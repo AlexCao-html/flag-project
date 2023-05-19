cb7c50de59493854b1b014438d67364edd4f32ec
AlexCao-html <bubocao@outlook.com>
2023-05-18 23:12:19 -0700

    did the flags until the 25 star flag (more than halfway done)

--- script.js
+++ script.js
@@ -1,24 +1,76 @@
-$("#Grand-Union-Flag").click(() => {
-    $("main section").css("display", "none")
-    $("#Grand-Union-Flag-Sect").css("display", "grid")
+document.querySelector("#Grand-Union-Flag").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Grand-Union-Flag-Sect").style.display = "grid"
 })
 
-$("#Pine-Tree-Flag").click(() => {
-    $("main section").css("display", "none")
-    $("#Pine-Tree-Flag-Sect").css("display", "grid")
+document.querySelector("#Pine-Tree-Flag").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Pine-Tree-Flag-Sect").style.display = "grid"
 })
 
-$("#13-stars").click(() => {
-    $("main section").css("display", "none")
-    $("#13-Star-Sect").css("display", "grid")
+document.querySelector("#Main-13-stars").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Main-13-Star-Sect").style.display = "grid"
 })
 
-$("#Battle-of-Bennington").click(() => {
-    $("main section").css("display", "none")
-    $("#Battle-of-Bennington-Sect").css("display", "grid")
+document.querySelector("#Battle-of-Bennington").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Battle-of-Bennington-Sect").style.display = "grid"
 })
 
-$("#15-stars").click(() => {
-    $("main section").css("display", "none")
-    $("#15-Star-Sect").css("display", "grid")
+document.querySelector("#Main-15-stars").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Main-15-Star-Sect").style.display = "grid"
+})
+
+document.querySelector("#Main-20-stars").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Main-20-Star-Sect").style.display = "grid"
+})
+
+document.querySelector("#Main-21-stars").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Main-21-Star-Sect").style.display = "grid"
+})
+
+document.querySelector("#Main-23-stars").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Main-23-Star-Sect").style.display = "grid"
+})
+
+document.querySelector("#Main-24-stars").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Main-24-Star-Sect").style.display = "grid"
+})
+
+document.querySelector("#Main-25-stars").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Main-25-Star-Sect").style.display = "grid"
+})
+
+document.querySelector("#Main-50-stars").addEventListener("click", () => {
+    for (var i = 0; i < document.querySelectorAll("main section").length; i++) {
+        document.querySelectorAll("main section")[i].style.display = "none"
+    }
+    document.querySelector("#Main-50-Star-Sect").style.display = "grid"
 })
